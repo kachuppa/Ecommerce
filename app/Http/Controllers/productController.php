dd9cb2880fbeaf2d517aea5afae7b8b4d0cbebfb
kachuppa <rairajesh2552@gmail.com>
2021-06-30 18:33:17 +0545

    login with session done plus migrated user data

---
+++ app/Http/Controllers/productController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class productController extends Controller
+{
+    //
+    function product()
+    {
+        return "Welcome to Product page";
+    }
+}
